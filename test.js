6ebea591483ae140ac608dfa2684d59a99d9494a
Vaughn <vrichard712@gmail.com>
2023-01-25 18:34:59 -0600

    cleaned function up a bit

--- test.js
+++ test.js
@@ -13,8 +13,6 @@ function drawCircle(opacity, svg, cx, cy, radius) {
         .style("opacity", opacity)
 }
 
-var width = 900;
-var height = 500;
 // this function takes in the latitude, longitude and returns its svg x,y coord
 // figue our xscale, yscale later
 const centerLat = 41.881832; const centerLong = -87.623177;
@@ -33,6 +31,9 @@ var svg = d3.select("body")
 var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
 
+var width = 900;
+var height = 500;
+
 d3.json("files/Boundaries - Neighborhoods.geojson", function(err, geojson) { 
 
       projection.fitSize([width,height],geojson); // adjust the projection to the features
@@ -43,49 +44,18 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-/*var projection = d3.geoMercator()
-    .scale((width-4) / (Math.PI / 5))
-    .translate([width / 2, height / 2])*/
-    //.center([-87.554420,41.739685]);
 
 var path = d3.geoPath()
     .projection(projection);
 
 var graticule = d3.geoGraticule();
 
-
-/*svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("id", "sphere")
-    .attr("d", path);*/
-
-/*svg.append("use")
-    .attr("class", "stroke")
-    .attr("xlink:href", "#sphere");*/
-
-/*svg.append("use")
-    .attr("class", "fill")
-    .attr("xlink:href", "#sphere");*/
-
 svg.append("path")
     .datum(graticule)
     .attr("class", "graticule")
     .attr("d", path);
 
-d3.json("files/chineighborhoods.json", function(error, chicago) {
-  if (error) throw error;
 
 var testAdjust = adjustCoordinate(41.881832,-87.623177, 1, 1, 500, 100);
 
 drawCircle(.5, svg, testAdjust[0], testAdjust[1], 20);
-
-  /*svg.insert("path", ".graticule")
-      .datum(topojson.feature(chicago, chicago.objects.land))
-      .attr("class", "land")
-      .attr("d", path);*/
-
-  /*svg.insert("path", ".graticule")
-      .datum(topojson.mesh(chicago, chicago.objects.chicagocommunityareas, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);*/
-});
